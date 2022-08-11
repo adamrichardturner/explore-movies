16eaa0eb4285859d9e98d5826fb70943561c9470
Adam Turner <hello@adamrichardturner.dev>
2022-08-11 10:38:06 +0100

    Added og data for social media sharing

--- src/app/App.js
+++ src/app/App.js
@@ -9,10 +9,26 @@ import { Trending } from '../features/Trending/Trending';
 import { Favourites } from '../features/Favourites/Favourites';
 import { Search } from '../features/Search/Search';
 import { SearchFailure } from '../features/Search/SearchFailure';
+import { Head } from 'react-static';
 
 const App = () => {
+  const openGraphData = {
+    title: 'Explore Movies | TMdb movie explorer app',
+    description:
+      'TMdb app for browsing popular and trending movies worldwide',
+    url: 'https://exploremovies.app/',
+    image:
+      '%PUBLIC_URL%/explore-movies-social.jpg',
+  }
   return (
     <div className="App" style={{height: '100vh'}}>
+      <Head>
+        <meta property="og:title" content={openGraphData.title} />
+        <meta property="og:description" content={openGraphData.description} />
+        <meta property="og:url" content={openGraphData.url} />
+        <meta property="og:image" content={openGraphData.image} />
+        <meta name="twitter:card" content="summary" />
+      </Head>
       <nav>
         <MiniDrawer />
       </nav>
